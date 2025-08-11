5b0ee535a277f21d701a17f6abe1915cd45d64e0
Aniket <moteaniket952@gmail.com>
2025-08-12 00:00:31 +0530

    accordian

--- src/components/ItemList.js
+++ src/components/ItemList.js
@@ -1,5 +1,14 @@
-const ItemList = ()=>{
-    return (
-        <div></div>
-    )
-}
+const ItemList = ({ items }) => {
+  console.log(items);
+  return (
+    <div className="my-5">
+      {items.map(({ card: { info } }) => (
+        <div className="flex justify-between px-4 py-5 shadow-lg rounded-lg" key={info.id}>
+          <span className="font-bold">{info.name}</span>
+          <span>{info.price ? info.price / 100 : "N/A"}$</span>
+        </div>
+      ))}
+    </div>
+  );
+};
+export default ItemList;
--- src/components/RestaurantCategory.js
+++ src/components/RestaurantCategory.js
@@ -1,13 +1,28 @@
+import { useState } from "react";
+import ItemCards from "./ItemList";
+
 const RestaurantCategory = ({ data }) => {
   console.log("props ", data);
+  const [isVisible, setIsVisible] = useState(false);
   return (
     <div>
-      <div className="w-6/12 mx-auto my-5 bg-gray-300 shadow-lg flex justify-between p-4 rounded-lg">
+      <div
+        onClick={() => {
+          isVisible === true ? setIsVisible(false) : setIsVisible(true);
+          console.log(isVisible);
+        }}
+        className="w-6/12 mx-auto my-4 bg-gray-300 shadow-lg flex justify-between p-4 rounded-lg hover:cursor-pointer"
+      >
         <span className="font-bold text-lg">
-            {data.title} ({data.itemCards.length})
+          {data.title} ({data.itemCards.length})
         </span>
         <span>⬇️</span>
       </div>
+      {isVisible && (
+        <div className="w-6/12 mx-auto bg-gray-100">
+          <ItemCards items={data.itemCards} />
+        </div>
+      )}
     </div>
   );
 };
